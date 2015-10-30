c30b08cdbbf0560fc28dc3cee5ae960fd328786d
StupidPreadeep <pradeeptiwari.bhumca10@gmail.com>
2015-10-30 22:29:18 +0530

    first commit

---
+++ upload/admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/dbCommerce/upload/admin/');
+define('HTTP_CATALOG', 'http://localhost/dbCommerce/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/dbCommerce/upload/admin/');
+define('HTTPS_CATALOG', 'http://localhost/dbCommerce/upload/');
+
+// DIR
+define('DIR_APPLICATION', '/opt/lampp/htdocs/dbCommerce/upload/admin/');
+define('DIR_SYSTEM', '/opt/lampp/htdocs/dbCommerce/upload/system/');
+define('DIR_LANGUAGE', '/opt/lampp/htdocs/dbCommerce/upload/admin/language/');
+define('DIR_TEMPLATE', '/opt/lampp/htdocs/dbCommerce/upload/admin/view/template/');
+define('DIR_CONFIG', '/opt/lampp/htdocs/dbCommerce/upload/system/config/');
+define('DIR_IMAGE', '/opt/lampp/htdocs/dbCommerce/upload/image/');
+define('DIR_CACHE', '/opt/lampp/htdocs/dbCommerce/upload/system/cache/');
+define('DIR_DOWNLOAD', '/opt/lampp/htdocs/dbCommerce/upload/system/download/');
+define('DIR_UPLOAD', '/opt/lampp/htdocs/dbCommerce/upload/system/upload/');
+define('DIR_LOGS', '/opt/lampp/htdocs/dbCommerce/upload/system/logs/');
+define('DIR_MODIFICATION', '/opt/lampp/htdocs/dbCommerce/upload/system/modification/');
+define('DIR_CATALOG', '/opt/lampp/htdocs/dbCommerce/upload/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'dbCommerce');
+define('DB_PREFIX', 'oc_');
